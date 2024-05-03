ef9e7c9284e0986695c0c639aac8ff9335e1d14e
Felipe Trujillo <trujillo.felipe1997@gmail.com>
2024-05-03 13:32:54 -0400

    e

--- src/print.ts
+++ src/print.ts
@@ -4,7 +4,7 @@ const { PosPrinter } = pkg
 export const newPrint = (data: any) => {
     const options = {
         preview: true,
-        margin: '0 0 0 20px',
+        margin: '0 30px 0 0',
         copies: 1,
         printerName: 'XP-58',
         boolean: true,
