f55b125b385c0f1ecbc293cd4fa52ea05756ee99
Felipe Trujillo <trujillo.felipe1997@gmail.com>
2024-05-03 14:04:53 -0400

    e

--- src/print.ts
+++ src/print.ts
@@ -1,4 +1,5 @@
 import pkg from 'electron-pos-printer'
+
 const { PosPrinter } = pkg
 
 export const newPrint = (data: any) => {
@@ -6,29 +7,11 @@ export const newPrint = (data: any) => {
         preview: false,
         margin: '0 30px 0 0',
         copies: 1,
-        printerName: 'XP-58',
+        printerName: '',
         boolean: true,
         timeOutPerLine: 400,
         pageSize: '58mm' as const, // page size
     }
-
-    /* const data = [
-        {
-            type: 'text' as const, // 'text' | 'barCode' | 'qrCode' | 'image' | 'table
-            value: 'SAMPLE HEADING',
-            style: { fontWeight: '700', textAlign: 'center', fontSize: '24px' },
-        },
-        {
-            type: 'text' as const, // 'text' | 'barCode' | 'qrCode' | 'image' | 'table'
-            value: 'Secondary text',
-            style: {
-                textDecoration: 'underline',
-                fontSize: '10px',
-                textAlign: 'center',
-                color: 'red',
-            },
-        },
-    ] */
 
     PosPrinter.print(data, options)
         .then(console.log)
