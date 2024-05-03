598bab19443f326704d6a5d26f4d109900be1d73
Felipe Trujillo <trujillo.felipe1997@gmail.com>
2024-05-03 13:34:40 -0400

    e

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,14 @@ server.use(bodyParser.json())
 server.post('/api/message', (req, res) => {
     const { message } = req.body
     console.log('Received message:', message)
-    newPrint(message)
+
+    try {
+        newPrint(message)
+    } catch (error) {
+        console.log(error)
+    }
+
+    return res.json('ok')
 })
 
 export default server
